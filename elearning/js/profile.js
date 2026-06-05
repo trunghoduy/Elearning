375c314de451ef10873bf90f5c1aec9d59d95a53
duytrung <hoduytrung999@gmail.com>
2026-06-05 09:39:43 +0700

    Fix FE cho UI

--- elearning/js/profile.js
+++ elearning/js/profile.js
@@ -47,9 +47,59 @@ function  loadprofile() {
         document.getElementById('fullname').value=res.data.fullname;
         document.getElementById('address').value=res.data.address;
         document.getElementById('phone').value=res.data.phone;   
+        document.getElementById('emailSecu').value=res.data.email;
  }).catch(function(res){
     console.log(res)
     })
+}
+function validatePassword() {
+    let pass = document.getElementById("password").value;
+    let confirm = document.getElementById("cfpassword").value;
+
+    if (pass !== confirm) {
+        alert("Confirm password không trùng khớp!");
+        return false;  
+    }
+
+    return true;
+}
+function validatePassword() {
+    let pass = document.getElementById("password");
+    let confirm = document.getElementById("cfpassword");
+
+    if (pass.value !== confirm.value) {
+        confirm.style.border = "1px solid red";
+        return false;
+    } else {
+        confirm.style.border = "1px solid #ced4da";
+        return true;
+    }
+}
+function editUser(id) {
+
+    if (!validatePassword()) return;
+ console.log("trung"+id);
+    let email = document.getElementById("emailSecu").value;
+    let password = document.getElementById("password").value;
+
+    axios({
+        url: 'http://localhost:2404/api/user/edit/' + id,
+        method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem("acesssToken")
+        },
+        data: {
+            email: email,
+            password: password
+        }
+    })
+    .then(function (res) {
+        alert("Cập nhật thành công!");
+    })
+    .catch(function (err) {
+        console.log(err);
+        alert("Lỗi khi lưu thông tin!");
+    });
 }
 
     
